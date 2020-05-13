4c84ea3dcb750bab5d6e5cfcc7e516681aeacbdc
LinqiWang <865046239@qq.com>
2020-05-13 09:37:17 +0800

    ceshi

--- main.py
+++ main.py
@@ -4,3 +4,11 @@
 
 import os
 import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    img_path = r'‪F:\ocr\mtwi\T1._WBXtXdXXXXXXXX_!!0-item_pic.jpg.jpg'
+    image = cv2.imread(img_path,cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    h,w= image.shape[:]
+    # 仿射变换矩阵，缩小2倍
+    A1 = np.array([[0.5,0,0],[0,0.5,0]],np.float32)
